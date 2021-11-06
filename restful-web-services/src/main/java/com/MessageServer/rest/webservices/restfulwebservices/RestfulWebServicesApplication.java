00848e610ae5097afd229498230b1611d20da9a2
baraklagziel <br.lagziel@gmail.com>
2021-11-06 09:21:21 +0200

    handle exception

--- restful-web-services/src/main/java/com/MessageServer/rest/webservices/restfulwebservices/RestfulWebServicesApplication.java
+++ restful-web-services/src/main/java/com/MessageServer/rest/webservices/restfulwebservices/RestfulWebServicesApplication.java
@@ -1,12 +1,15 @@
 package com.MessageServer.rest.webservices.restfulwebservices;
 
+import com.MessageServer.rest.webservices.restfulwebservices.exception.BadRequestException;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.io.IOException;
 
 @SpringBootApplication
 public class RestfulWebServicesApplication {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws BadRequestException, IOException, InterruptedException {
 		SpringApplication.run(RestfulWebServicesApplication.class, args);
 	}
 
